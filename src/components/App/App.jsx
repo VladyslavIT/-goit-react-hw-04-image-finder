992b8152fff577a7e0cb3b612cb4030fb965c756
VladyslavIT <vlad.chernyy.1997@gmail.com>
2022-11-03 00:58:07 +0200

    fixed all

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,81 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
+import { fetchImages } from 'Api/Api';
 
 import { Searchbar } from '../Searchbar/Searchbar';
+import { Loader } from 'components/Loader/Loader';
+import { Button } from 'components/Button/Button';
 import { ImageGallery } from '../ImageGallery/ImageGallery';
 import { Container } from './App.styled';
 
-
-
-
 const App = () => {
-  
-  const [findName, setFindName] = useState('');
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [image, setImage] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState('idle');
+  const [total, setTotal] = useState(null);
 
   const formSubmit = name => {
-    setFindName(name);
-   
-  }
+    setQuery(name);
+    setPage(1);
+  };
+
+  useEffect(() => {
+    if (query === '') {
+      return;
+    }
+
+    if (page > 1) {
+      fetchImages(query, page).then(response => {
+        setImage(prevImage => [...prevImage, ...response.hits]);
+        setLoading(false);
+        setStatus('resolve');
+      });
+      return;
+    }
+
+    setLoading(true);
+
+    fetchImages(query, page).then(response => {
+      if (response.hits.length === 0) {
+        toast.error('Nothing as requested, please try another word');
+        setImage([]);
+        setLoading(false);
+        setStatus('pending');
+        return;
+      }
+
+      setImage([...response.hits]);
+      setLoading(false);
+      setPage(1);
+      setStatus('resolve');
+      setTotal(response.total);
+      toast.success('Successful search');
+    });
+  }, [query, page]);
+
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
 
   return (
-       <Container>
-        <Searchbar onSubmit={formSubmit} />
-        <ImageGallery nameGallery={findName} />
-        <ToastContainer autoClose={2000} />
-      </Container>
-  )
-}
-
-
+    <Container>
+      <Searchbar onSubmit={formSubmit} />
+      {status === 'rejected' && <p>Oops, try reloading the page</p>}
+      {loading && <Loader />}
+      {status === 'resolve' && <ImageGallery images={image} />}
+      {image.length > 0 && image.length !== total ? (
+        <Button onClick={loadMore} />
+      ) : (
+        <></>
+      )}
+      <ToastContainer autoClose={2000} />
+    </Container>
+  );
+};
 
 export { App };
-
-
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,89 +1,33 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ToastContainer } from 'react-toastify';
-import { toast } from 'react-toastify';
 
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { Loader } from 'components/Loader/Loader';
-import { Button } from 'components/Button/Button';
 import { ImageGalley } from './ImageGallary.styled';
-import { fetchImages } from 'Api/Api';
 
-const ImageGallery = ({ nameGallery }) => {
-  const [image, setImage] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState('idle');
-  const [page, setPage] = useState(1);
-  const [total, setTotal] = useState(null);
-
-  useEffect(() => {
-    if (!nameGallery || nameGallery === '') {
-      return;
-    }
-
-    if (page > 1 ) {
-      fetchImages(nameGallery, page).then(response => {
-        setImage(prevImage => [...prevImage, ...response.hits]);
-        setLoading(false);
-        setStatus('resolve');
-      });
-      return;
-    }
-
-    setLoading(true);
-
-    fetchImages(nameGallery, page).then(response => {
-      if (response.hits.length === 0) {
-        toast.error('Nothing as requested, please try another word');
-        setImage([]);
-        setLoading(false);
-        setStatus('pending');
-        return;
-      }
-
-      setImage([...response.hits]);
-      setLoading(false);
-      setPage(1);
-      setStatus('resolve');
-      setTotal(response.total);
-      toast.success('Successful search');
-    });
-  }, [nameGallery, page]);
-
-  const loadMore = () => {
-    setPage(prevPage => prevPage + 1);
-  };
-
-  return (
-    <>
-      {status === 'rejected' && <p> {error}</p>}
-      {loading && <Loader />}
-      {status === 'resolve' && (
-        <ImageGalley>
-          {image.map(({ id, webformatURL, largeImageURL, tags }) => (
-            <ImageGalleryItem
-              key={id}
-              webformatURL={webformatURL}
-              largeImageURL={largeImageURL}
-              tags={tags}
-            />
-          ))}
-          <ToastContainer />
-        </ImageGalley>
-      )}
-      {image.length > 0 && image.length !== total ? (
-        <Button onClick={loadMore} />
-      ) : (
-        <></>
-      )}
-    </>
-  );
-};
+const ImageGallery = ({ images }) => (
+  <ImageGalley>
+    {images.map(({ id, webformatURL, largeImageURL, tags }) => (
+      <ImageGalleryItem
+        key={id}
+        webformatURL={webformatURL}
+        largeImageURL={largeImageURL}
+        tags={tags}
+      />
+    ))}
+    <ToastContainer />
+  </ImageGalley>
+);
 
 ImageGallery.propTypes = {
-  nameGallery: PropTypes.string.isRequired,
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+      tags: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
 };
 
 export { ImageGallery };
